722ad74fa764c53f5fcd57bb45be80b006295e86
Nilton Hiroyuki Matsumoto <hiroyukims@hotmail.com>
2022-03-31 15:59:12 -0300

    remove comments

--- optimized-perceptron.py
+++ optimized-perceptron.py
@@ -1,34 +1,34 @@
-# Supervised Learning
-# Classification algorithm
-
 import numpy as np
 
-inputs = np.array([0,0],[0,1],[1,0],[1,1])
+inputs = np.array([0, 0], [0, 1], [1, 0], [1, 1])
 outputs = np.array([0, 0, 0, 1])
 weights = np.array([0.0, 0.0])
 larningRate = 0.1
 
-def stepFunction(sum) : 
-    if(sum >= 1) :
+
+def stepFunction(sum):
+    if(sum >= 1):
         return 1
     return 0
 
-def calcOutput(register) :
+
+def calcOutput(register):
     sum = register.dot(weights)
     return stepFunction(sum)
 
-# not optimized, only teaching purposes
-def train() : 
+
+def train():
     totalErrors = 1
-    while(totalErrors != 0) : 
+    while(totalErrors != 0):
         totalErrors = 0
-        for i in range(len(outputs)) :
+        for i in range(len(outputs)):
             calculetedOutput = calcOutput(np.asarray(inputs[i]))
             error = abs(outputs[i] - calculetedOutput)
-            totalErrors +=  error
+            totalErrors += error
             # update weights
-            for j in range(len(weights)) : 
-                weights[j] = weights[j] + (larningRate * inputs[i][j] * error) 
+            for j in range(len(weights)):
+                weights[j] = weights[j] + (larningRate * inputs[i][j] * error)
     return 0
+
 
 train()
